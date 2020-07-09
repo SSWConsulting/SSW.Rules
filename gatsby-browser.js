0d07f2eac6c702285cfc2d4c01688fe31e8371c4
TiSeb <boissiere.seb@gmail.com>
2020-07-09 17:55:03 +1000

    Good/bad examples with text

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,7 @@
 import wrapPageElementWithTransition from './src/helpers/wrapPageElement.js';
 
+require('prismjs/themes/prism-okaidia.css');
+require('prismjs/plugins/line-numbers/prism-line-numbers.css');
+
 // Page Transitions
 export const wrapPageElement = wrapPageElementWithTransition;
--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: 'gatsby-source-git',
       options: {
         name: 'categories',
-        remote: 'https://github.com/SSWConsulting/SSW.Rules.git',
+        remote: 'https://github.com/SSWConsulting/SSW.Rules.Content.git',
         // Optionally supply a branch. If none supplied, you'll get the default branch.
         //53120-CreateIndexTemplate
         //branch: 'content-migration-staging',
@@ -47,6 +47,71 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              // Class prefix for <pre> tags containing syntax highlighting;
+              // defaults to 'language-' (e.g. <pre class="language-js">).
+              // If your site loads Prism into the browser at runtime,
+              // (e.g. for use with libraries like react-live),
+              // you may use this to prevent Prism from re-processing syntax.
+              // This is an uncommon use-case though;
+              // If you're unsure, it's best to use the default value.
+              classPrefix: 'language-',
+              // This is used to allow setting a language for inline code
+              // (i.e. single backticks) by creating a separator.
+              // This separator is a string and will do no white-space
+              // stripping.
+              // A suggested value for English speakers is the non-ascii
+              // character '›'.
+              inlineCodeMarker: null,
+              // This lets you set up language aliases.  For example,
+              // setting this to '{ sh: "bash" }' will let you use
+              // the language "sh" which will highlight using the
+              // bash highlighter.
+              aliases: {},
+              // This toggles the display of line numbers globally alongside the code.
+              // To use it, add the following line in gatsby-browser.js
+              // right after importing the prism color scheme:
+              //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+              // Defaults to false.
+              // If you wish to only show line numbers on certain code blocks,
+              // leave false and use the {numberLines: true} syntax below
+              showLineNumbers: false,
+              // If setting this to true, the parser won't handle and highlight inline
+              // code used in markdown i.e. single backtick code like `this`.
+              noInlineHighlight: false,
+              // This adds a new language definition to Prism or extend an already
+              // existing language definition. More details on this option can be
+              // found under the header "Add new language definition or extend an
+              // existing language" below.
+              languageExtensions: [
+                {
+                  language: 'superscript',
+                  extend: 'javascript',
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              // Customize the prompt used in shell output
+              // Values below are default
+              prompt: {
+                user: 'root',
+                host: 'localhost',
+                global: false,
+              },
+              // By default the HTML entities <>&'" are escaped.
+              // Add additional HTML escapes by providing a mapping
+              // of HTML entities and their escape value IE: { '}': '&#123;' }
+              escapeEntities: {},
+            },
+          },
           {
             resolve: 'gatsby-remark-figure-caption',
           },
@@ -84,14 +149,17 @@ module.exports = {
                 imgSm: {
                   classes: 'img-small',
                 },
-                imgGood: {
-                  classes: 'img-good-example',
+                goodExample: {
+                  classes: 'good-example',
+                  title: 'optional',
                 },
-                imgBad: {
-                  classes: 'img-bad-example',
+                badExample: {
+                  classes: 'bad-example',
+                  title: 'optional',
                 },
-                imgOK: {
-                  classes: 'img-ok-example',
+                oKExample: {
+                  classes: 'ok-example',
+                  title: 'optional',
                 },
               },
             },
@@ -107,7 +175,7 @@ module.exports = {
               urlOverrides: [
                 {
                   id: 'youtube',
-                  embedURL: videoId =>
+                  embedURL: (videoId) =>
                     `https://www.youtube-nocookie.com/embed/${videoId}`,
                 },
               ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support

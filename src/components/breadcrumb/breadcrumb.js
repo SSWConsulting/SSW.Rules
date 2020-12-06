3d81b678247c0e5bd6c89f8b42db89c28a043dee
tombui99 <tom.quang.bui@gmail.com>
2020-12-07 10:56:58 +1100

    Fixed breadcrumb ssw logo navigate back to parentsite

--- src/components/breadcrumb/breadcrumb.js
+++ src/components/breadcrumb/breadcrumb.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Icon from '../../images/icon.png';
+import { parentSiteUrl } from '../../../site-config';
 
 const Breadcrumbs = (props) => {
   return (
     <div className="breadcrumb-container">
       <div className="mx-6 mb-3 breadcrumb">
-        <a href={'/'}>
+        <a href={parentSiteUrl}>
           <img alt={'SSW Consulting'} src={Icon} className="w-4" />
         </a>
 

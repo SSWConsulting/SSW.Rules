8981685decd3fb167fc20a34b2368c34ab9342b8
Brady Stroud <bradystroud@me.com>
2021-01-06 11:33:20 +1000

    🚨  Remove unused import

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,6 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
-import { withPrefix } from 'gatsby';
 
 const SignIn = () => {
   const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();

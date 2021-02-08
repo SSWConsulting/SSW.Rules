5148998e13a31fddc1c2bed741fb6a90a75c6df9
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2021-02-08 10:55:56 +1000

    ✨  Add comments to rules (#218)

---
+++ src/components/comments/comments.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { commentsRepositry, commentsTheme } from '../../../site-config';
+
+export default class Comments extends Component {
+  constructor(props) {
+    super(props);
+    this.commentBox = React.createRef();
+  }
+
+  componentDidMount() {
+    let scriptEl = document.createElement('script');
+    scriptEl.setAttribute('src', 'https://utteranc.es/client.js');
+    scriptEl.setAttribute('crossorigin', 'anonymous');
+    scriptEl.setAttribute('async', true);
+    scriptEl.setAttribute('repo', commentsRepositry);
+    scriptEl.setAttribute('issue-term', this.props.guid);
+    scriptEl.setAttribute('theme', commentsTheme);
+    this.commentBox.current.appendChild(scriptEl);
+  }
+
+  render() {
+    return (
+      <div className="comment-box-wrapper container pt-70">
+        <hr className="comment-seperator" />
+        <div ref={this.commentBox} className="comment-box" />
+      </div>
+    );
+  }
+}
+
+Comments.propTypes = {
+  guid: PropTypes.string,
+};

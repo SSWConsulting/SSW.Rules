0740cf6a1e605926a4613c60b1d7866ecb73e89a
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2021-02-16 14:05:25 +1000

    💄 Move dropdown avatar (#278)
    * 💄 Move dropdown avatar
    
    * 💄  Make dropdown wider

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,14 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
 /* eslint-disable no-console */
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
 import DropdownIcon from '../dropdown-icon/dropdown-icon';
 import {
   GetGithubOrganisations,
   setUserOrganisation,
 } from '../../services/apiService';
 
-const SignIn = ({ displayActions }) => {
+const SignIn = () => {
   const {
     isAuthenticated,
     loginWithRedirect,
@@ -62,15 +61,11 @@ const SignIn = ({ displayActions }) => {
         </button>
       ) : (
         <>
-          <DropdownIcon displayActions={displayActions} />
+          <DropdownIcon />
         </>
       )}
     </div>
   );
-};
-
-SignIn.propTypes = {
-  displayActions: PropTypes.bool,
 };
 
 export default SignIn;

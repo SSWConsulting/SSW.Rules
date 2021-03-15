0a707261b91b8c0aa79be01f7bd6a216c43c9caf
Brady Stroud <bradystroud@me.com>
2021-03-15 17:13:49 +1000

    ✨  Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,25 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
-const NotFound = () => <span>Not found.</span>;
+const NotFound = () => {
+  return (
+    <div className="not-found-page">
+      <div className="not-found-grid">
+        <h1>404</h1>
+        <div className="not-found-message">
+          <h2>
+            Sorry, the page you're looking for has been found...{' '}
+            <b>NOT FOUND!</b>
+          </h2>
+          <p>
+            Visit our <Link to="/">Rules Index</Link> to find out more secret
+            ingredients to quality software or <Link to="/">go back</Link> to
+            the previous page.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default NotFound;

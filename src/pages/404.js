bce93297563d3ce661bdba277b994854b4b906d2
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2021-03-17 12:33:03 +1000

    60062 404 page (#395)
    * ✨  Add 404 page
    
    * 💄  Fix 404 page
    
    * ✨  Made 'back' button work
    
    * 💄  Fix bookmark hover text

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,26 @@
+import { Link, navigate } from 'gatsby';
 import React from 'react';
 
-const NotFound = () => <span>Not found.</span>;
+const NotFound = () => {
+  return (
+    <div className="not-found-page">
+      <div className="not-found-grid">
+        <h1>404</h1>
+        <div className="not-found-message">
+          <h2>
+            Sorry, the page you&#39;re looking for has been found...{' '}
+            <span>NOT FOUND!</span>
+          </h2>
+          <p>
+            Visit our <Link to="/">Rules Index</Link> to find out more secret
+            ingredients to quality software or{' '}
+            <button onClick={() => navigate(-1)}>go back</button> to the
+            previous page.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default NotFound;

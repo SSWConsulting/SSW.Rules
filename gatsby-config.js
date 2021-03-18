697f0793ee38b18a5701768fbfb0ac7391f311b3
tombui99 <tom.quang.bui@gmail.com>
2021-03-19 10:58:29 +1100

    Added gatsby plugin to embed Facebook post

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,49 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-pinterest-twitter-facebook`,
+      options: {
+        // Use this to control how long you want this plugin to wait before initializing the Facebook and Twitter scripts
+        // By default, this plugin will wait 1 second before initializing the social widgets to ensure that the page loads and the container elements for these widgets are created in the DOM
+        // Value should be in milliseconds
+        delayTimer: 1000, // default
+        facebook: {
+          // Set this to false if you want to disable facebook
+          enable: true, // default
+          // Set this to the querySelector string of the container element where you want to display facebook timeline and like button
+          // If you do not define this, facebook timeline and like button will not work
+          // Fox example: `.facebook-container` or `#.facebook-timeline`
+          containerSelector: null,
+          // Set this to the facebook profile or facebook page name you want to use
+          // If you do not define this, facebook timeline and like button will not work
+          // Do not include `@` symbol here
+          profile: null, // default
+          // The pixel width of the plugin as a number
+          // Do not include units such as 'px', 'rem', '%' etc here
+          // Minimum allowed value is 180 and Maximum allowed value is 500
+          // If you leave this as null, the plugin will try to occupy all available container width
+          width: null, // default
+          // The pixel height of the plugin as a number
+          // Do not include units such as 'px', 'rem', '%' etc here
+          // Minimum allowed value is 70
+          // If you leave this as null, the plugin will try to occupy all available container height
+          height: null, // default
+          // Tabs to render i.e. timeline, events, messages. Use a comma-separated list to add multiple tabs, i.e. timeline, events.
+          tabs: 'timeline, events, messages', // default
+          // Hide cover photo in the header
+          hideCover: false, // default
+          // Show profile photos when friends like this, i.e. if you are logged in to Facebook, it will show the photos of your friends who have liked this page
+          showFacepile: true, // default
+          // Use the small header instead, i.e show a smaller cover photo
+          smallHeader: false, // default
+          // Try to fit inside the container width on page load
+          // If the width of the parent element is bigger than the Page plugin's width it will maintain the value defined in width
+          // No Dynamic Resizing - If you want to adjust the plugin's width on window resize, you manually need to rerender the plugin.
+          adaptContainerWidth: true, // default
+        },
+      },
+    },
+    {
       resolve: 'gatsby-source-git',
       options: {
         name: 'categories',

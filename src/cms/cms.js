fb95b104d9eca317f3dce1ba989c80ee99b27871
Brady Stroud <bradystroud@me.com>
2021-04-23 12:45:06 +1000

    Fix styles

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,6 @@
-import CMS from 'netlify-cms-app';
+import CMS from 'netlify-cms';
 import PostPreview from './previewTemplate';
-
 import styles from '!css-loader!../style.css';
 
 CMS.registerPreviewStyle(styles.toString(), { raw: true });
-
 CMS.registerPreviewTemplate('rule', PostPreview);

f7200133e75d368acd11613fce77f545efa3cbe8
Brady Stroud <bradystroud@me.com>
2021-04-23 14:03:42 +1000

    Use newer CMS package

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,8 @@
-import CMS from 'netlify-cms';
+import CMS from 'netlify-cms-app';
 import PostPreview from './previewTemplate';
 import styles from '!css-loader!../style.css';
+
+CMS.init();
 
 CMS.registerPreviewStyle(styles.toString(), { raw: true });
 CMS.registerPreviewTemplate('rule', PostPreview);

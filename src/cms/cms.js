1208eb41b2fab9ef43a54033d6a9d260d8171c70
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2021-06-07 10:14:46 +1000

    Connect Netlify CMS to authentication functions (#550)
    * Use staging functions for Netlify
    
    * ♻️  Use env vars for cms
    
    * 🐛  Fix content not loading

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,13 @@ import CMS from 'netlify-cms-app';
 import PostPreview from './previewTemplate';
 import styles from '!css-loader!../style.css';
 
-CMS.init();
+CMS.init({
+  config: {
+    backend: {
+      base_url: process.env.API_BASE_URL,
+    },
+  },
+});
 
 CMS.registerPreviewStyle(styles.toString(), { raw: true });
 CMS.registerPreviewTemplate('rule', PostPreview);

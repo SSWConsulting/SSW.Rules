ebd6fb1ac97493d63a6502c7be241d5bde91bfd4
Taine Riley <40375803+taineriley1@users.noreply.github.com>
2021-07-08 17:37:46 +1000

    59378 - Changed styling of breadcrumbs for consistency (#585)
    * Changed styling of breadcrumbs for consistency
    
    * Changed styling of breadcrumbs for consistency
    
    * 🚨 Fixed linting errors
    
    * 💄 Changed font colours for consistency
    
    * Removed accidental style change
    
    Co-authored-by: tjriley10 <t.riley.10@student.scu.edu.au>

--- src/components/breadcrumb/breadcrumb.js
+++ src/components/breadcrumb/breadcrumb.js
@@ -27,25 +27,28 @@ const Breadcrumbs = (props) => {
           <img alt={'SSW Consulting'} src={Icon} className="w-4" />
         </a>
 
-        <span className="breadcrumb__separator">&gt;</span>
+        <span className="pl-1 breadcrumb__separator">&gt;</span>
 
-        <a className="breadcrumb-content px-1" href={siteUrl}>
-          SSW Rules
-        </a>
+        <Link ref={linkRef} to={siteUrl}>
+          <div className="breadcrumb-content px-1 hover:text-red-600">
+            SSW Rules
+          </div>
+        </Link>
 
-        <div className="px-1">
+        <span className="px-1">
           {props.isCategory || props.isRule || props.isArchived ? '>' : ''}
-        </div>
+        </span>
         {props.categories && (
           <div className="text-left ">{getCategories()}</div>
         )}
         {props.isArchived ? (
-          <div className="px-1 text-gray-900">Archived</div>
+          <div className="px-1">Archived</div>
         ) : props.isCategory ? (
-          <div className="px-1 text-gray-900">{props.categoryTitle}</div>
+          <div className="px-1">{props.categoryTitle}</div>
         ) : (
-          <div className="px-1 text-gray-900">
-            {props.isHomePage ? '' : '>'} {props.title}
+          <div>
+            <span className="px-1">{props.isHomePage ? '' : '>'}</span>
+            {props.title}
           </div>
         )}
       </div>

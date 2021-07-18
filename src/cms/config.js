059a451a93cf72420cf99206c82cbf59a4ad87ef
Taine Riley <taineriley@ssw.com.au>
2021-07-19 08:45:22 +1000

    🚨 Fixed linter issues

--- src/cms/config.js
+++ src/cms/config.js
@@ -48,7 +48,7 @@ var configJson = {
             label: 'uri (a unique identifier for the url)',
             pattern: [
               '^[a-zA-Z0-9_-]*$',
-              "Must only contain alphanumeric or the '-' and '_' characters",
+              'Must only contain alphanumeric or the "-" and "_" characters',
             ],
           },
           {
@@ -79,7 +79,7 @@ var configJson = {
               label: 'Rule uri',
               pattern: [
                 '^[a-zA-Z0-9_-]*$',
-                "Must only contain alphanumeric or the '-' and '_' characters",
+                'Must only contain alphanumeric or the "-" and "_" characters',
               ],
             },
           },
@@ -93,7 +93,7 @@ var configJson = {
               label: 'Redirect uri',
               pattern: [
                 '^[a-zA-Z0-9_-]*$',
-                "Must only contain alphanumeric or the '-' and '_' characters",
+                'Must only contain alphanumeric or the "-" and "_" characters',
               ],
             },
           },

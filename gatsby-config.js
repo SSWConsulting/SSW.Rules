e0a8ce23471955d2cec0ce8391b2f2b271f7f264
Dale Chester <77970786+theixdev@users.noreply.github.com>
2021-11-08 14:39:13 +1000

    ✨ Display lightboxes on image clicks. (#725)
    * Added Image Anywhere
    
    * Fixed-  Zoom working for all images.
    
    * Image Zoom working
    
    * Added background transparency
    
    * lint fix
    
    * Removed accidental check in
    
    * Package experiment

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,11 +72,50 @@ module.exports = {
       },
     },
     'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
         excerpt_separator: '<!--endintro-->',
         plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images-v2',
+          },
+          // {
+          //   resolve: 'gatsby-remark-images-anywhere',
+          //   options: {
+          //     staticDir: 'static',
+          //     createMarkup: ({
+          //       src,
+          //       srcSet,
+          //       sizes,
+          //       aspectRatio,
+          //       alt,
+          //       base64,
+          //       presentationWidth,
+          //     }) => {
+          //       return `<img src="${src}" class="gatsby-resp-image-image medium-zoom-image"  srcset="${srcSet}" sizes="${sizes}" aspectratio="${aspectRatio}" alt="${alt}" base64="${base64}" presentationwidth="${presentationWidth}"></img`;
+          //     },
+          //     sharpMethod: 'fluid',
+          //     // Additional sharp image arguments: https://www.gatsbyjs.org/packages/gatsby-plugin-sharp/
+          //     // maxWidth: 650,
+          //   },
+          // },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              linkImagesToOriginal: false,
+              maxWidth: 1232,
+              backgroundColor: 'transparent',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images-zoom',
+            options: { background: 'rgba(50,50,50,0.9)' },
+          },
           {
             resolve: '@raae/gatsby-remark-oembed',
             options: {
@@ -106,22 +145,6 @@ module.exports = {
           {
             resolve: 'gatsby-remark-figure-caption',
             options: { figureClassName: 'image' },
-          },
-          // gatsby-remark-relative-images must
-          // go before gatsby-remark-images
-          {
-            resolve: 'gatsby-remark-relative-images-v2',
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              //linkImagesToOriginal: false,
-              maxWidth: 1232,
-              backgroundColor: 'transparent',
-            },
           },
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-fenced-divs',

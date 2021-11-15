849faf7e80fca1b7000a3d30bf713f180dbb03ed
Christian Morford-Waite [SSW] <ChristianMWaite@ssw.com.au>
2021-11-15 17:22:03 +1100

    Update get-rule-history.ps1

--- .azure/scripts/get-rule-history.ps1
+++ .azure/scripts/get-rule-history.ps1
@@ -7,7 +7,7 @@ param (
 cd SSW.Rules/
 
 #GenerateHistoryFile - Read Data from Azure function and save to JSON
-$Uri = $AzFunctionBaseUrl + 'GenerateHistoryFileFunction'
+$Uri = $AzFunctionBaseUrl + '/api/GenerateHistoryFileFunction'
 $Headers = @{'x-functions-key' = $GenerateHistoryFileFunctionKey}
 $Response = Invoke-WebRequest -URI $Uri -Headers $Headers
 $Response.Content | Out-File -FilePath ./$outputFileName

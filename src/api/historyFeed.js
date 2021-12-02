94a03040838969dcff592c3611796f65f1201add
Tylah Kapa <TylahKapa@gmail.com>
2021-12-02 16:45:06 +1000

    Update api call

--- src/api/historyFeed.js
+++ src/api/historyFeed.js
@@ -4,7 +4,7 @@ export default async function handle(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   //TODO: Have Brady implement error handling
-  fs.readJson('public/history-feed.json').then((response) => {
+  fs.readJson('history-feed.json').then((response) => {
     res.status(200).json(response);
   });
 }

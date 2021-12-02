eb1bee0ab8c07f80ee14772ababfe82ab1c6860e
Tylah Kapa [SSW] <tylahkapa@gmail.com>
2021-12-02 12:15:04 +1000

    Serve history feed (#776)
    * ✨ Building a history feed of rules
    
    * Build history feed
    
    * remove console log

---
+++ src/helpers/historyFeedGenerator.js
@@ -0,0 +1,60 @@
+const fs = require('fs-extra');
+
+const createHistoryFeed = async (pluginData, pages, graphql) => {
+  const { publicFolder } = pluginData;
+  const FILE_PATH = publicFolder('history-feed.json');
+  const pageData = Array.from(pages.values()).map((page) => {
+    return {
+      path: page.path,
+      id: page.context.id,
+      file: page.context.file,
+      title: page.context.title,
+      uri: page.context.uri,
+    };
+  });
+
+  const result = await graphql(`
+    query {
+      history: allHistoryJson(
+        sort: { fields: lastUpdated, order: DESC }
+        filter: { file: { glob: "rules/**" } }
+        limit: 10
+      ) {
+        edges {
+          node {
+            id
+            file
+            lastUpdated
+            lastUpdatedBy
+            lastUpdatedByEmail
+          }
+        }
+      }
+    }
+  `);
+
+  //TODO: Fix this code block
+  const nodes = Array.from(
+    result.data.history.edges.map(({ node }) => {
+      var currentPage = pageData.find((x) => node.file == x.file);
+
+      return {
+        id: node.id,
+        lastUpdated: node.lastUpdated,
+        lastUpdatedBy: node.lastUpdatedBy,
+        lastUpdatedByEmail: node.lastUpdatedByEmail,
+        file: node.file,
+        title: currentPage?.title ?? 'No title',
+        uri: currentPage?.uri ?? 'not-found',
+      };
+    })
+  );
+
+  const data = `[ ${nodes.map((x) => JSON.stringify(x)).join(',')} ]`;
+
+  return fs.writeFile(FILE_PATH, data);
+};
+
+module.exports = {
+  createHistoryFeed,
+};

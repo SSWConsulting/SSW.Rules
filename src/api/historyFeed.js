a836c0fad70f8fb81610f0c754bc54e62bd58842
Tylah Kapa [SSW] <tylahkapa@gmail.com>
2021-12-07 13:22:38 +1000

    Delete historyFeed.js

--- src/api/historyFeed.js
+++
@@ -1,9 +0,0 @@
-export default async function handle(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  res.status(200).json({ message: 'Hello, World!' });
-  //TODO: Have Brady implement error handling
-  // fs.readJson('history-feed.json').then((response) => {
-  //   res.status(200).json(response);
-  // });
-}

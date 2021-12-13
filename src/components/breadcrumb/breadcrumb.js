cc272d4d67cf0b1c9e1a0e089e8b45c619aedcff
JackDevAU <jpr178@live.com.au>
2021-12-13 11:23:46 +1000

    💄 slight css changes + remove container classes

--- src/components/breadcrumb/breadcrumb.js
+++ src/components/breadcrumb/breadcrumb.js
@@ -1,14 +1,14 @@
-import React, { useRef } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
 import './breadcrumb.css';
-import Icon from '../../images/icon.png';
+
+import React, { useRef } from 'react';
 import { parentSiteUrl, siteUrl } from '../../../site-config';
+
+import Icon from '../../images/icon.png';
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
 
 const Breadcrumbs = (props) => {
   const getCategories = () => {
-    props.categories.push({ link: '/', title: 'This is another category' });
-    props.categories.push({ link: '/', title: 'This is another category' });
     return props.categories.map((cat, i) => {
       return (
         <Link key={i} to={cat.link}>
@@ -20,89 +20,27 @@ const Breadcrumbs = (props) => {
 
   return (
     <div className="breadcrumb-container">
-      <div>
-        <Link to={parentSiteUrl}>
-          <img alt="SSW Foursquare" src={Icon} className="w-4" />
-        </Link>
-      </div>
-      <div>
-        <ul className="mx-4 breadcrumb">
-          <li>
-            <Link to={siteUrl} className="breadcrumb-content">
-              SSW Rules
-            </Link>
-          </li>
-          {props.categories && (
-            <li className="breadcrumb-category">{getCategories()}</li>
-          )}
-          {props.isArchived ? (
-            <li>Archived</li>
-          ) : props.isCategory ? (
-            <li>{props.categoryTitle}</li>
-          ) : (
-            <li>{props.title}</li>
-          )}
-        </ul>
-      </div>
+      <Link to={parentSiteUrl}>
+        <img alt="SSW Foursquare" src={Icon} className="w-4" />
+      </Link>
+      <ul className="breadcrumb">
+        <li>
+          <Link to={siteUrl} className="breadcrumb-content">
+            SSW Rules
+          </Link>
+        </li>
+        {props.categories && (
+          <li className="breadcrumb-category">{getCategories()}</li>
+        )}
+        {props.isArchived ? (
+          <li>Archived</li>
+        ) : props.isCategory ? (
+          <li>{props.categoryTitle}</li>
+        ) : (
+          <li>{props.title}</li>
+        )}
+      </ul>
     </div>
-
-    // <div className="breadcrumb-container">
-    //   <div className="mx-4 mb-4 breadcrumb">
-    //     <a href={parentSiteUrl}>
-    //       <img
-    //         alt={'SSW Consulting'}
-    //         src={Icon}
-    //         className="w-4"
-    //         style={{ marginTop: '0px' }}
-    //       />
-    //     </a>
-
-    //     <div>
-    //       <ul>
-    //         <li>
-    //           <Link ref={linkRef} to={siteUrl} className="breadcrumb-content">
-    //             SSW Rules
-    //           </Link>
-    //         </li>
-    //         {props.categories && <li>{getCategories()}</li>}
-    //         {props.isArchived ? (
-    //           <li>Archived</li>
-    //         ) : props.isCategory ? (
-    //           <li>{props.categoryTitle}</li>
-    //         ) : (
-    //           <li>{props.title}</li>
-    //         )}
-    //       </ul>
-    //     </div>
-
-    //     {
-    //       //TODO: Remove Dead Code
-    //       /* <span className="pl-1 breadcrumb__separator">&gt;</span>
-    //     <Link ref={linkRef} to={siteUrl}>
-    //       <div className="breadcrumb-content px-1 hover:text-red-600">
-    //         SSW Rules
-    //       </div>
-    //     </Link>
-
-    //     <span className="px-1">
-    //       {props.isCategory || props.isRule || props.isArchived ? '>' : ''}
-    //     </span>
-    //     {props.categories && (
-    //       <div className="text-left ">{getCategories()}</div>
-    //     )}
-    //     {props.isArchived ? (
-    //       <div className="px-1">Archived</div>
-    //     ) : props.isCategory ? (
-    //       <div className="px-1">{props.categoryTitle}</div>
-    //     ) : (
-    //       <div>
-    //         <span className="px-1">{props.isHomePage ? '' : '>'}</span>
-    //         {props.title}
-    //       </div>
-    //     )} */
-    //     }
-    //   </div>
-    // </div>
   );
 };
 

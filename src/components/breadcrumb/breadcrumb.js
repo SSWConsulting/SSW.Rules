14aeafa17670df33e92acdfdb00b1c6e5fbf8bf0
JackDevAU <jpr178@live.com.au>
2021-12-13 15:27:04 +1000

    💄 more style fixes

--- src/components/breadcrumb/breadcrumb.js
+++ src/components/breadcrumb/breadcrumb.js
@@ -11,7 +11,7 @@ const Breadcrumbs = (props) => {
   const getCategories = () => {
     return props.categories.map((cat, i) => {
       return (
-        <Link key={i} to={cat.link} className="flex-1 mr-4">
+        <Link key={i} to={cat.link} className="flex-1">
           {cat.title}
         </Link>
       );
@@ -24,19 +24,19 @@ const Breadcrumbs = (props) => {
     }
 
     if (props.isCategory) {
-      return <li>{props.categoryTitle}</li>;
+      return <li className="mr-8">{props.categoryTitle}</li>;
     }
 
-    return <li>{props.title}</li>;
+    return <li className="mr-8">{props.title}</li>;
   };
 
   return (
-    <div className="w-full m-4 md:mx-12 mx-8 h-full flex min-w-full">
+    <div className="w-full m-4 mx-8 md:mx-2 h-full flex min-w-full">
       <Link to={parentSiteUrl} className="mx-2 h-full">
         <img alt="SSW Foursquare" src={Icon} className="w-4 mt-1" />
       </Link>
       <div className="w-full">
-        <ul className="flex flex-col md:flex-row align-middle breadcrumb ">
+        <ul className="flex flex-col md:flex-row align-middle breadcrumb mr-8">
           <li className="w-auto flex">
             <Link to={siteUrl} className="flex-1 align-middle">
               SSW Rules

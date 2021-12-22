fc59fafc2384a56cd11d47febf590c62ddfbb406
Brady Stroud <bradystroud@ssw.com.au>
2021-12-22 14:54:41 +1000

    ⚡️ Make a few changes to script

--- .azure/scripts/update-rule-history.ps1
+++ .azure/scripts/update-rule-history.ps1
@@ -9,43 +9,41 @@ param (
 
 $ErrorActionPreference = 'Stop'
 
-cd SSW.Rules.Content/
+Set-Location SSW.Rules.Content/
 
 #Step 0: Prep the Repo for git log
 git commit-graph write --reachable --changed-paths
 
 #Step 1: GetHistorySyncCommitHash - Retrieve CommitHash from AzureFunction and make GitHub Requests
 $Uri = $AzFunctionBaseUrl + 'GetHistorySyncCommitHash'
-$Headers = @{'x-functions-key' = $GetHistorySyncCommitHashKey}
+$Headers = @{'x-functions-key' = $GetHistorySyncCommitHashKey }
 $Response = Invoke-WebRequest -URI $Uri -Headers $Headers
 $startCommitHash = $Response.Content
+$ShortStartCommitHash = $startCommitHash.substring(0, 9)
 
 $gitHubRequest = $null
-$test = $null
 $pageNumber = 1
 $found = $false
 $obj = @()
 
-while($null -eq $test){    
+while (-not $found) {    
     Write-Output "GitHub Request: $($pageNumber)"
     $endpoint = "https://api.github.com/repos/SSWConsulting/SSW.Rules.Content/commits?page=$($pageNumber)&per_page=100"
 
-    $gitHubRequest += Invoke-RestMethod -Method 'Get' -Uri $endpoint -Headers @{"Authorization"="Bearer $($gitHubPAT)"}
-    
+    $gitHubRequest += Invoke-RestMethod -Method 'Get' -Uri $endpoint -Headers @{"Authorization" = "Bearer $($gitHubPAT)" }
+
     Foreach ($res in $gitHubRequest) {
-        $shortSha = $res.sha.substring(0,9) 
+        $shortSha = $res.sha.substring(0, 9)        
         $user = $res.author.login
-        if($shortSha -eq $startCommitHash)
-        {
+        if ($shortSha -eq $ShortStartCommitHash) {
             $found = $true
+            $obj += New-Object PSObject -Property @{
+                User     = $user
+                ShortSha = $shortSha
+            }
+            break
         }
-        $obj += New-Object PSObject -Property @{
-            User = $user
-            ShortSha = $shortSha
-        }
-    
     }
-    $test = $found
 
     ++$pageNumber
 }
@@ -64,8 +62,7 @@ $historyArray | Foreach-Object {
     $userDetails = $historyEntry[0] -split "<LINE>"
     $fileArray = $historyEntry[1] -split "<LINE>"
 
-    if($commitSyncHash -eq "" -and $userDetails[1].Length -gt 5)
-    {
+    if ($commitSyncHash -eq "" -and $userDetails[1].Length -gt 5) {
         $commitSyncHash = $userDetails[1]
     }
 
@@ -74,28 +71,27 @@ $historyArray | Foreach-Object {
     $lastUpdatedByEmail = $userDetails[4]
     $commitHash = $userDetails[5]
     
-    $fileArray | Where-Object {$_ -Match "^*.md" } | Foreach-Object {
-        if(!$filesProcessed.ContainsKey($_))
-        {
+    $fileArray | Where-Object { $_ -Match "^*.md" } | Foreach-Object {
+        if (!$filesProcessed.ContainsKey($_)) {
             $createdRecord = git log --diff-filter=A --reverse --pretty="%ad<LINE>%aN<LINE>%ae" --date=iso-strict -- $_
             $createdDetails = $createdRecord -split "<LINE>"
-
-            $commit = $obj | Where-Object ShortSha -eq $commitHash
+            $shortCommitHash = $commitHash.substring(0, 9)
+            $commit = $obj | Where-Object ShortSha -eq $shortCommitHash #This wont work when HEAD is used...
             $gitHubUser = $commit.User
 
             $filesProcessed.Add($_, 0)
             $historyFileArray += @{
-                file = $($_)
-                lastUpdated = $lastUpdated
-                lastUpdatedBy = $lastUpdatedBy
+                file                  = $($_)
+                lastUpdated           = $lastUpdated
+                lastUpdatedBy         = $lastUpdatedBy
                 lastUpdatedByUsername = $gitHubUser
-                lastUpdatedByEmail = $lastUpdatedByEmail
-                created = $createdDetails[0]
-                createdBy = $createdDetails[1]
-                createdByEmail = $createdDetails[2]
+                lastUpdatedByEmail    = $lastUpdatedByEmail
+                created               = $createdDetails[0]
+                createdBy             = $createdDetails[1]
+                createdByEmail        = $createdDetails[2]
             }
 
-            echo $_
+            Write-Output $_
         }
     }
 }
@@ -104,19 +100,18 @@ $historyFileContents = ConvertTo-Json $historyFileArray
 
 #Step 3: UpdateRuleHistory - Send History Patch to AzureFunction
 $Uri = $AzFunctionBaseUrl + 'UpdateRuleHistory'
-$Headers = @{'x-functions-key' = $UpdateRuleHistoryKey}
+$Headers = @{'x-functions-key' = $UpdateRuleHistoryKey }
 $Response = Invoke-WebRequest -Uri $Uri -Method Post -Body $historyFileContents -Headers $Headers -ContentType 'application/json; charset=utf-8'
 
-if(![string]::IsNullOrWhiteSpace($commitSyncHash))
-{
+if (![string]::IsNullOrWhiteSpace($commitSyncHash)) {
     #Step 4: UpdateHistorySyncCommitHash - Update Commit Hash with AzureFunction
     $Uri = $AzFunctionBaseUrl + 'UpdateHistorySyncCommitHash'
-    $Headers = @{'x-functions-key' = $UpdateHistorySyncCommitHashKey}
+    $Headers = @{'x-functions-key' = $UpdateHistorySyncCommitHashKey }
     $Body = @{
-        commitHash  = $commitSyncHash
+        commitHash = $commitSyncHash
     }
-    $Result = Invoke-WebRequest -Uri $Uri -Method Post -Body $Body -Headers $Headers
+    Invoke-WebRequest -Uri $Uri -Method Post -Body $Body -Headers $Headers
 }
 
-echo $historyFileContents
-echo $commitSyncHash
+Write-Output $historyFileContents
+Write-Output $commitSyncHash

14c14325a4f6196e4a397f2d75db84a6fc9d2de9
Brady Stroud <bradystroud@ssw.com.au>
2022-04-11 08:23:20 +1000

    🎨  Format code

--- src/components/side-bar/side-bar.js
+++ src/components/side-bar/side-bar.js
@@ -87,10 +87,9 @@ const SideBar = ({ ruleTotalNumber, location }) => {
         <p>
           SSW Consulting has over 30 years of experience developing awesome
           Microsoft solutions that today build on top of Angular, React, Azure,
-          Azure DevOps, SharePoint, Office 365, .NET Core, WebAPI,
-          Dynamics 365, and SQL Server. With 40+ consultants in 5 countries, we
-          have delivered the best in the business to more than 1,000 clients in
-          15 countries.
+          Azure DevOps, SharePoint, Office 365, .NET Core, WebAPI, Dynamics 365,
+          and SQL Server. With 40+ consultants in 5 countries, we have delivered
+          the best in the business to more than 1,000 clients in 15 countries.
         </p>
       </section>
       <section>

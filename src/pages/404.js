a2fbe56bb896a2e1d34fd5657837803a11c76a1f
Tiago Araújo [SSW] <tiagov8@gmail.com>
2022-06-22 03:39:47 -0700

    💄 Improvements to 404 page (#871)
    * Improvements to 404 page
    
    * 🚨  Lint changes
    
    Co-authored-by: Brady Stroud <bradystroud@ssw.com.au>

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,18 +8,18 @@ const NotFound = () => {
         <h1 className="unselectable">404</h1>
         <div className="not-found-message">
           <h2 className="unselectable">
-            The verdict is in... <p />
+            The verdict is in... <br />
             SSW Rules has concluded that this page is <span>NOT FOUND!</span>
           </h2>
         </div>
         <div className="not-found-greybox greybox">
-          Visit our <Link to="/">Rules home page</Link> to find out more secret
-          ingredients to quality software or{' '}
+          Visit <Link to="/">SSW Rules homepage</Link> and find out more secret
+          ingredients to quality software; or{' '}
           <button onClick={() => navigate(-1)}>go back</button> to the previous
           page.
         </div>
         <div className="not-found-greybox greybox">
-          See the Rule{' '}
+          This page is as per{' '}
           <a href="https://ssw.com.au/rules/404-useful-error-page">
             Do you replace the 404 error with a useful error page?
           </a>

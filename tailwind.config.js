ee1cccca01360bedc37a818459aecfef1a9116ff
ChloeLin <chloelin@ssw.com.au>
2023-04-25 11:30:24 +0800

    replaced css with tailwind classname

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./src/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xs: '0.8rem',
+      },
+      colors: {
+        'gray-tooltip': '#9e9e9e',
+      },
+    },
   },
   plugins: [],
 };

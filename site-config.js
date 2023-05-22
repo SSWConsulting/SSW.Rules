9249766cceb221e76c110ac479312a8ef5a1aa3d
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-05-23 08:48:18 +1000

    Updated breadcrumbs (#970)
    * Updated breadcrumbs
    - Links now relative
    - Added new 'siteUrlRelative' attribute in config file
    
    * Added build script to update 'siteRelativeUrl'
    - Script updates 'siteRelativeUrl' attribute from `/` to `/rules` on build
    
    * added comment to new site-config attribute

--- site-config.js
+++ site-config.js
@@ -5,6 +5,8 @@ module.exports = {
   siteTitleShort: `SSW.Rules`,
   siteDescription: `Secret Ingredients to Quality Software | SSW Rules provides best practices for developing secure, reliable, and efficient .NET, Azure, CRM, Angular, React, Dynamics, and AI applications. Learn more today!`,
   siteUrl: `https://www.ssw.com.au/rules`,
+  // Relative URL to website home
+  siteUrlRelative: `/`,
   themeColor: `#cc4141`,
   backgroundColor: `#fff`,
   pathPrefix: `/rules`,

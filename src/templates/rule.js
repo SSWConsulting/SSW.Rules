1b3cb0cb1b6d25ff67406ca78c57a71e1ae6a1c2
Chloe Lin [SSW] <chloelin@ssw.com.au>
2023-05-25 10:23:16 +0800

    ✨ Add tag for archived rule in category section (#975)
    * Add "Archived" tag
    
    * Replace tag css with tailwind , delete unused css

--- src/templates/rule.js
+++ src/templates/rule.js
@@ -378,7 +378,7 @@ const Rule = ({ data, location }) => {
               </div>
               {categories.map((category, i) => (
                 <div className="inline px-1" key={i}>
-                  <span className="category-tag">
+                  <span className="bg-ssw-red transition-colors duration-250 ease-in px-1.5 py-0.5 my-1 text-xs rounded inline-block cursor-pointer hover:opacity-80">
                     <Link ref={linkRef} to={`/${category.parent.name}`}>
                       {category.frontmatter.title
                         .replace('Rules to Better ', '')
@@ -387,6 +387,15 @@ const Rule = ({ data, location }) => {
                   </span>
                 </div>
               ))}
+              {rule.frontmatter.archivedreason?.length > 0 && (
+                <div className="inline px-1">
+                  <span className="bg-ssw-red transition-colors duration-250 ease-in px-1.5 py-0.5 my-1 text-xs rounded inline-block cursor-pointer hover:opacity-80">
+                    <Link ref={linkRef} to={'/archived'}>
+                      Archived
+                    </Link>
+                  </span>
+                </div>
+              )}
             </div>
 
             <div className="likes w-full  lg:w-1/3">

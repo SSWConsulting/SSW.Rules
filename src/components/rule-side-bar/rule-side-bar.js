721ffcde2e5839c53d4b3c8acd1a7eea14e15c05
ChloeLin <chloelin@ssw.com.au>
2023-06-12 14:00:41 +0800

    Remove info icons

--- src/components/rule-side-bar/rule-side-bar.js
+++ src/components/rule-side-bar/rule-side-bar.js
@@ -4,8 +4,6 @@ import Categories from '../sidebar-categories/sidebar-categories';
 import RelatedRules from '../related-rules/related-rules';
 import PropTypes from 'prop-types';
 import Tooltip from '../tooltip/tooltip';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 const tooltipData = {
   Authors: {
@@ -22,23 +20,20 @@ const SidebarHeader = ({ sectionTitle }) => {
   const tooltip = tooltipData[sectionTitle];
 
   return (
-    <div className="flex mt-4 text-center before:mr-5 after:ml-5 before:mb-7 after:mb-7 before:content-[''] before:flex-1 before:border-b before:border-solid after:flex-1 after:border-b after:border-solid">
-      <h5 className="text-ssw-red text-xl ">{sectionTitle}</h5>
-
-      {tooltip && (
-        <div className="flex items-center [&>button]:h-6 pl-1">
+    <div className="flex pt-9 mb-4 text-center before:mr-5 after:ml-5 before:mb-3 after:mb-3 before:content-[''] before:flex-1 before:border-b before:border-solid after:flex-1 after:border-b after:border-solid">
+      <div className="flex items-center [&>button]:h-6 pl-1">
+        {tooltip ? (
           <Tooltip text={tooltip.text}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={tooltip.link}
-              className="hover:scale-110 pt-0.5"
-            >
-              <FontAwesomeIcon icon={faInfoCircle} size="sm" />
+            <a target="_blank" rel="noopener noreferrer" href={tooltip.link}>
+              <h5 className="text-ssw-red text-xl">{sectionTitle}</h5>
             </a>
           </Tooltip>
-        </div>
-      )}
+        ) : (
+          <button className="flex h-10 items-center justify-center">
+            <h5 className="text-ssw-red text-xl">{sectionTitle}</h5>
+          </button>
+        )}
+      </div>
     </div>
   );
 };

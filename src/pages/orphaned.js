4114fcea580227cf2fd7dbe2b8496e80a19c2d53
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-06-20 10:07:12 +1000

    Added warning to edit tooltip due to Netlify issues (#1018)

--- src/pages/orphaned.js
+++ src/pages/orphaned.js
@@ -205,7 +205,13 @@ const Orphaned = ({ data }) => {
                                     className="bookmark-icon"
                                   />
                                 </a>
-                                <span className="tooltiptext">Edit</span>
+                                <span className="tooltiptext w-52 !-left-[4.6rem]">
+                                  Edit
+                                  <p>
+                                    (Warning: Stale branches can cause issues -
+                                    See wiki for help)
+                                  </p>
+                                </span>
                               </button>
                               <button className="tooltip">
                                 <a

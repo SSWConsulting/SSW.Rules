06f36e1da18657c65951233b44c601a04efe43ea
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-07-04 13:55:13 +1000

    Fixed casing

--- src/templates/rule.js
+++ src/templates/rule.js
@@ -173,7 +173,7 @@ const Rule = ({ data, location }) => {
                           )}`}
                           href={`https://github.com/SSWConsulting/SSW.Rules.Content/commits/${process.env.CONTENT_BRANCH}/rules/${rule.frontmatter.uri}/rule.md`}
                         >
-                          See History
+                          See history
                         </a>
                       </small>
                     )}

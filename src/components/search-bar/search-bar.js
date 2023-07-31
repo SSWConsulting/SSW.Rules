d0c44558f789566a2cb30bb252737ee5c82fc228
ChloeLin <chloelin@ssw.com.au>
2023-07-31 17:03:26 +0800

    refactor search-bar to load json file

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -1,15 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FlexSearch from 'flexsearch';
 import PropTypes from 'prop-types';
+import qs from 'query-string';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-const SearchBar = ({ searchQuery, setSearchQuery, toSearch }) => {
-  const handlePressEnter = (val) => {
-    if (!toSearch || !searchQuery) return;
+const SearchBar = ({
+  toSearch,
+  publicIndexURL,
+  publicStoreURL,
+  setSearchResult,
+  location,
+}) => {
+  const [query, setQuery] = useState('');
+  const [searchIndex, setSearchIndex] = useState(null);
+  const [searchStore, setSearchStore] = useState(null);
+  const [index, setIndex] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
+  useEffect(() => {
+    const searchString = qs.parse(location?.search).keyword;
+    setQuery(searchString);
+
+    const fetchData = async () => {
+      const indexResponse = await fetch(publicIndexURL);
+      const indexData = await indexResponse.text();
+      setSearchIndex(indexData);
+
+      const storeResponse = await fetch(publicStoreURL);
+      const storeData = await storeResponse.json();
+      setSearchStore(storeData);
+
+      setIsLoaded(true);
+    };
+
+    fetchData();
+  }, []);
+
+  const handlePressEnter = (val) => {
+    if (!toSearch) return;
     const pathPrefix = process.env.NODE_ENV === 'development' ? '' : '/rules';
     window.location.href = `${pathPrefix}/search?keyword=${val}`;
   };
+
+  const fetchSearch = async () => {
+    if (!query || !index || !searchStore) return [];
+    const rawResults = index.search(query);
+
+    setSearchResult(rawResults.map((id) => searchStore[id]));
+  };
+
+  useEffect(() => {
+    if (isLoaded) {
+      fetchSearch();
+    }
+  }, [query, isLoaded]);
+
+  useEffect(() => {
+    if (!searchIndex) {
+      setIndex(null);
+      return;
+    }
+    if (searchIndex instanceof FlexSearch) {
+      setIndex(searchIndex);
+      return;
+    }
+
+    const importedIndex = FlexSearch.create();
+    importedIndex.import(searchIndex);
+
+    setIndex(importedIndex);
+  }, [searchIndex]);
 
   return (
     <div className="border border-solid w-96 ml-4 flex items-center pl-3 p-2 rounded shadow bg-gray-50">
@@ -19,8 +80,8 @@ const SearchBar = ({ searchQuery, setSearchQuery, toSearch }) => {
         className="text-gray-400 mr-2"
       />
       <input
-        value={searchQuery}
-        onInput={(e) => setSearchQuery(e.target.value)}
+        value={query}
+        onInput={(e) => setQuery(e.target.value)}
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
             handlePressEnter(e.target.value);
@@ -37,7 +98,9 @@ const SearchBar = ({ searchQuery, setSearchQuery, toSearch }) => {
 export default SearchBar;
 
 SearchBar.propTypes = {
-  searchQuery: PropTypes.string.isRequired,
-  setSearchQuery: PropTypes.func.isRequired,
   toSearch: PropTypes.bool,
+  publicIndexURL: PropTypes.string,
+  publicStoreURL: PropTypes.string,
+  setSearchResult: PropTypes.func,
+  location: PropTypes.object,
 };

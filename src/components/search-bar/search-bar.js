5136ab242eb3e71805f261dcfdd164386bf4c03f
ChloeLin <chloelin@ssw.com.au>
2023-07-31 17:40:39 +0800

    fix eslint

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -102,8 +102,10 @@ export default SearchBar;
 
 SearchBar.propTypes = {
   toSearch: PropTypes.bool,
+  isLoaded: PropTypes.bool,
   publicIndexURL: PropTypes.string,
   publicStoreURL: PropTypes.string,
   setSearchResult: PropTypes.func,
+  setIsLoaded: PropTypes.func,
   location: PropTypes.object,
 };

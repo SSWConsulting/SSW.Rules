4a702b5efe0e7f206a81f6dcfc87c566b50d4890
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-10-30 15:27:38 +1000

    remove playwright in favor for remark-draw (#1142) (#1146)
    * added commands to build playwright
    
    * removed useless file
    
    * increasing timeout limit
    
    * testing remark-draw
    
    * removed playwright stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,27 +91,16 @@ module.exports = {
     {
       resolve: 'gatsby-transformer-remark',
       options: {
+        strategy: 'img',
         excerpt_separator: '<!--endintro-->',
         plugins: [
           {
-            resolve: 'gatsby-remark-mermaid',
-            options: /** @type {import('gatsby-remark-mermaid').Options} */ ({
-              mermaidConfig: {
+            resolve: 'gatsby-remark-draw',
+            options: {
+              mermaid: {
                 theme: 'neutral',
-                // TODO: Re-implement once light-mode is supported - https://github.com/SSWConsulting/SSW.Mermaid/blob/main/master-theme.txt
-                // theme: 'base',
-                // themeVariables: {
-                //   primaryColor: '#CC4141',
-                //   primaryTextColor: '#fff',
-                //   lineColor: '#CC4141',
-                //   secondaryColor: '#AAAAAA',
-                //   tertiaryColor: '#797979',
-                //   noteBkgColor: '#333333',
-                //   noteTextColor: '#fff',
-                //   sequenceNumberColor: '#333333',
-                // },
               },
-            }),
+            },
           },
           {
             resolve: '@raae/gatsby-remark-oembed',

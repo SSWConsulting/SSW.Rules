4c7349ee7ccd28a699d31f2522e80429a6d443d8
Kristen <kristenhu@ssw.com.au>
2024-04-25 16:53:50 +0800

    ✨CMS editor - add a link as tips

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,26 @@
 window.CMS_MANUAL_INIT = true;
 
+import React from 'react';
 import CMS from 'decap-cms-app';
 import PostPreview from './previewTemplate';
 import styles from '!css-loader!../style.css';
 import configJson from './config.js';
+
+const tipsControl = () => {
+  return (
+    <div>
+      <a
+        target="_blank"
+        href="https://github.com/SSWConsulting/SSW.Rules.Content/wiki/Netlify-CMS-%F0%9F%90%9B----Failed-to-save"
+        rel="noreferrer"
+      >
+        HOW TO FIX API ERROR 502
+      </a>
+    </div>
+  );
+};
+
+CMS.registerWidget('tips', tipsControl);
 
 CMS.init(configJson);
 

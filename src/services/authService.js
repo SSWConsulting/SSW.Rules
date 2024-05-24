6200fe2665fb7626490e3be27c889646b197e142
ChloeLin <chloelin@ssw.com.au>
2024-05-24 14:49:13 +0800

    fix order

--- src/services/authService.js
+++ src/services/authService.js
@@ -14,13 +14,13 @@ export const useAuthService = () => {
           audience: process.env.AUTH0_AUDIENCE,
           scope: 'openid profile email offline_access',
           grant_type: 'refresh_token',
-          ignoreCache: true,
           cacheMode: 'off',
         });
-        return claims.__raw;
-      } else {
-        return claims.__raw;
+
+        const refreshedClaims = await getIdTokenClaims();
+        return refreshedClaims.__raw;
       }
+      return claims.__raw;
     } catch (error) {
       console.error('Error refreshing ID token:', error);
       throw error;

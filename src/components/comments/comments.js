5e762117701067ff953f72bea24f394490db87c0
igorgoldobin <IgorGoldobin@ssw.com.au>
2024-06-08 17:51:01 +1000

    feat: added giscus integration

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -1,16 +1,31 @@
-import { Disqus } from 'gatsby-plugin-disqus';
+import { Giscus } from '@giscus/react';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Comments = (props) => {
-  let disqusConfig = {
-    identifier: props.ruleGuid,
-    title: props.title,
-  };
+const GISCUS_REPO_ID = process.env.GISCUS_REPO_ID;
+
+const Comments = () => {
   return (
-    <div className="disqus-box">
-      <Disqus config={disqusConfig} />
-    </div>
+    <>
+      <div className="disqus-box">
+        <Giscus
+          id="comments"
+          repo="SSWConsulting/SSW.Rules"
+          repoId={GISCUS_REPO_ID}
+          category="General"
+          categoryId="DIC_kwDOD1jvwM4B_y_I"
+          mapping="pathname"
+          term="Welcome to SSW Rules"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme="light"
+          lang="en"
+          loading="lazy"
+          async
+        />
+      </div>
+    </>
   );
 };
 

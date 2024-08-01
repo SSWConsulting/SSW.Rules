7d7b2331b5412d829729e2bf08791ef34e2388d9
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2024-08-01 16:03:01 +0930

    Add /rules/ to icon paths

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -109,83 +109,83 @@ const Head = ({
 
       <meta content={themeColor} name="msapplication-TileColor" />
       <meta
-        content="/icons/mstile-70x70.png"
+        content="/rules/icons/mstile-70x70.png"
         name="msapplication-square70x70"
       />
       <meta
-        content="/icons/mstile-144x144.png"
+        content="/rules/icons/mstile-144x144.png"
         name="msapplication-square144x144"
       />
       <meta
-        content="/icons/mstile-150x150.png"
+        content="/rules/icons/mstile-150x150.png"
         name="msapplication-square150x150"
       />
       <meta
-        content="/icons/mstile-310x150.png"
+        content="/rules/icons/mstile-310x150.png"
         name="msapplication-wide310x150"
       />
       <meta
-        content="/icons/mstile-310x310.png"
+        content="/rules/icons/mstile-310x310.png"
         name="msapplication-square310x310"
       />
 
       <link href={`${pathPrefix}/manifest.json`} rel="manifest" />
 
       <link
-        href="/icons/apple-touch-icon-57x57.png"
+        href="/rules/icons/apple-touch-icon-57x57.png"
         rel="apple-touch-icon"
         sizes="57x57"
       />
       <link
-        href="/icons/apple-touch-icon-60x60.png"
+        href="/rules/icons/apple-touch-icon-60x60.png"
         rel="apple-touch-icon"
         sizes="60x60"
       />
       <link
-        href="/icons/apple-touch-icon-72x72.png"
+        href="/rules/icons/apple-touch-icon-72x72.png"
         rel="apple-touch-icon"
         sizes="72x72"
       />
       <link
-        href="/icons/apple-touch-icon-76x76.png"
+        href="/rules/icons/apple-touch-icon-76x76.png"
         rel="apple-touch-icon"
         sizes="76x76"
       />
       <link
-        href="/icons/apple-touch-icon-114x114.png"
+        href="/rules/icons/apple-touch-icon-114x114.png"
         rel="apple-touch-icon"
         sizes="114x114"
       />
       <link
-        href="/icons/apple-touch-icon-120x120.png"
+        href="/rules/icons/apple-touch-icon-120x120.png"
         rel="apple-touch-icon"
         sizes="120x120"
       />
       <link
-        href="/icons/apple-touch-icon-144x144.png"
+        href="/rules/icons/apple-touch-icon-144x144.png"
         rel="apple-touch-icon"
         sizes="144x144"
       />
       <link
-        href="/icons/apple-touch-icon-152x152.png"
+        href="/rules/icons/apple-touch-icon-152x152.png"
         rel="apple-touch-icon"
         sizes="152x152"
       />
       <link
-        href="/icons/apple-touch-icon-180x180.png"
+        href="/rules/icons/apple-touch-icon-180x180.png"
         rel="icon"
         sizes="180x180"
         type="image/png"
       />
 
       <link
-        href="/icons/favicon-32x32.png"
+        href="/rules/icons/favicon-32x32.png"
         rel="icon"
         sizes="32x32"
         type="image/png"
       />
       <link
-        href="/icons/favicon-16x16.png"
+        href="/rules/icons/favicon-16x16.png"
         rel="icon"
         sizes="16x16"
         type="image/png"

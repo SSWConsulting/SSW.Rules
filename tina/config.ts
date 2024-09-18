69ed973fca43c7b26eb12ce944a4119f8578746c
Tylah Kapa <tylahkapa@gmail.com>
2024-09-19 08:51:41 +1000

    Set up Separate Repo Content

--- tina/config.ts
+++ tina/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tinacms';
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
+  process.env.CONTENT_BRANCH ||
   process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
@@ -10,13 +11,12 @@ const branch =
 export default defineConfig({
   // Required as per https://tina.io/docs/frameworks/gatsby/#workaround-for-graphql-mismatch-issue
   client: { skip: true },
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
 
   branch,
 
-  // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  localContentPath: '../../fork/SSW.Rules.Content',
 
   build: {
     outputFolder: 'admin',
@@ -25,7 +25,7 @@ export default defineConfig({
   media: {
     tina: {
       mediaRoot: '',
-      publicFolder: 'status',
+      publicFolder: 'static',
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
@@ -34,7 +34,7 @@ export default defineConfig({
       {
         name: 'post',
         label: 'Posts',
-        path: 'content/posts',
+        path: '/posts',
         fields: [
           {
             type: 'string',

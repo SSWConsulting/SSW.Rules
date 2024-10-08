4ce71c970341ab7763c1e1dc026af9c8752fd0a8
Tylah Kapa <tylahkapa@gmail.com>
2024-10-08 12:16:29 +1000

    Use TINA_ instead

--- tina/config.ts
+++ tina/config.ts
@@ -6,7 +6,7 @@ const branch = process.env.TINA_CONTENT_BRANCH ?? 'main';
 const localContentPath = process.env.LOCAL_CONTENT_RELATIVE_PATH ?? undefined;
 const clientId = process.env.TINA_CLIENT_ID;
 const token = process.env.TINA_TOKEN;
-const basePath = process.env.TINACMS_BASE_PATH ?? undefined;
+const basePath = process.env.TINA_BASE_PATH ?? undefined;
 
 export default defineConfig({
   // Required as per https://tina.io/docs/frameworks/gatsby/#workaround-for-graphql-mismatch-issue

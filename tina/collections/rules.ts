27801f1a55ee5d5c77a60fbf19976a6c8d8fdbea
isaaclombardssw <isaaclombard@ssw.com.au>
2024-10-08 15:00:56 +1100

    updating the rules schema

--- tina/collections/rules.ts
+++ tina/collections/rules.ts
@@ -1,36 +1,59 @@
 import { Collection } from "tinacms";
+import { generateGuid } from "../../src/helpers/generateGuid";
 
 export const Rules: Collection = {
   label: 'Rules',
   name: 'rules',
   path: '/rules',
   format: 'md',
+  defaultItem: () => {
+    return {
+      guid: generateGuid(),
+      created: new Date().toISOString(),
+      filename: 'rule',
+    }
+  },
+  ui: {
+    filename: {
+      readonly: true,
+    },
+  },
   fields: [
     {
       type: 'string',
       name: 'title',
       label: 'Title',
       required: true,
+      description: 'This is used as the major heading for the rule.',
     },
     {
       type: 'string',
       name: 'uri',
       label: 'Uri',
+      description: 'The URI of the rule – this defines the slug and refereces.',
       required: true,
     },
     {
       type: 'object',
       name: 'authors',
       label: 'Authors',
+      description: 'The list of contributors for this rule.',
       list: true,
+      ui: {
+        itemProps: (item) => {
+          return { label: '👤 ' + (item?.title ?? "Author") };
+        },
+      },
       fields: [
         {
           type: 'string',
           name: 'title',
+          description: 'The full name of the contributor, as it should appear on the rule.',
           label: 'Name',
         },
         {
           type: 'string',
+          description: 'The SSW People link for the contributor – e.g. https://ssw.com.au/people/sebastien-boissiere',
           name: 'url',
           label: 'Url',
         },
@@ -40,6 +63,7 @@ export const Rules: Collection = {
       type: 'string',
       label: 'Related Rules',
       name: 'related',
+      description: 'The URIs of rules that should be suggested based on the content of this rule.',
       list: true,
     },
     {
@@ -51,27 +75,40 @@ export const Rules: Collection = {
     {
       type: 'datetime',
       name: 'created',
+      description: 'If you see this field, contact a dev immediately 😳 (should be a hidden field generated in the background).',
       label: 'Created',
+      ui: {
+        component: 'hidden',
+
+      }
     },
     {
       type: 'string',
       name: 'archivedreason',
       label: 'Archived Reason',
+      description: 'If this rule has been archived, summarise why here.',
     },
     {
       type: 'string',
       name: 'guid',
       label: 'Guid',
+      description: 'If you see this field, contact a dev immediately 😳 (should be a hidden field generated in the background).',
+      ui: {
+        component: 'hidden',
+
+      }
     },
     {
       type: 'string',
       name: 'seoDescription',
       label: 'SEO Description',
+      description: 'A summary of the page content, used for SEO purposes. This can be generated automatically with AI.',
     },
     {
       type: 'rich-text',
       name: 'body',
       label: 'Body',
+      description: 'Rule content. This is where the magic happens.',
       isBody: true,
     },
   ],

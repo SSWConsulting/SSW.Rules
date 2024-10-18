a942df6785604b382e65119e4a4334b441663be7
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-10-18 16:54:06 +0800

    delete profiles pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -239,13 +239,6 @@ exports.createPages = async ({ graphql, actions }) => {
       });
     });
   });
-
-  const profilePage = require.resolve('./src/pages/profile.js');
-  createPage({
-    path: `${siteConfig.pathPrefix}/people/`,
-    matchPath: `${siteConfig.pathPrefix}/people/:gitHubUsername`,
-    component: profilePage,
-  });
 };
 
 exports.sourceNodes = async ({ actions, createNodeId }) => {

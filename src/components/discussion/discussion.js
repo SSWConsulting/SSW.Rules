bab57903b4cb95cff349778024317644322397b4
Babanazar Kamyljanov [SSW] <138548013+babakamyljanovssw@users.noreply.github.com>
2024-10-29 10:59:21 +0800

    Update src/components/discussion/discussion.js
    Co-authored-by: Brady Stroud [SSW] <bradystroud@ssw.com.au>

--- src/components/discussion/discussion.js
+++ src/components/discussion/discussion.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Discussion = (props) => {
   return (
     <div className="discussion-box">
-      <h3>Giscus comments</h3>
+      <h3>✨ Giscus comments</h3>
       <Giscus
         id="comments"
         repo="SSWConsulting/SSW.Rules.Content"

fea9174c77954b9ae23cedeeaebeda2d95aa5a9e
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-11-06 14:04:48 +0800

    replaced inter with inter-variable, added preload to avoid FOUT

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,21 @@
+/* eslint-disable react/react-in-jsx-scope */
+import * as React from 'react';
 import wrapPageElementWithTransition from './src/helpers/wrapPageElement.js';
+import '@fontsource-variable/inter';
+import interWoff2 from '@fontsource-variable/inter/files/inter-latin-wght-normal.woff2?url';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      rel="preload"
+      as="font"
+      type="font/woff2"
+      href={interWoff2}
+      crossOrigin="anonymous"
+      key="interPreload"
+    />,
+  ]);
+};
 
 // Page Transitions
 export const wrapPageElement = wrapPageElementWithTransition;

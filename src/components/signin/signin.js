47e05cbfd9de36a7ba23aada7c8fd6b467445f68
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-11-11 15:22:58 +0800

    removed secret content calls

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,43 +1,11 @@
 import { useAuth0 } from '@auth0/auth0-react';
 /* eslint-disable no-console */
-import React, { useEffect } from 'react';
+import React from 'react';
 import DropdownIcon from '../dropdown-icon/dropdown-icon';
-import {
-  GetGithubOrganisations,
-  setUserOrganisation,
-} from '../../services/apiService';
-import { useAuthService } from '../../services/authService';
-import useAppInsights from '../../hooks/useAppInsights';
 
 const SignIn = () => {
-  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
-  const { fetchIdToken } = useAuthService();
-  const { trackException } = useAppInsights();
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
 
-  useEffect(() => {
-    isAuthenticated ? setUserOrg() : null;
-  }, []);
-
-  const setUserOrg = async () => {
-    isAuthenticated
-      ? await GetGithubOrganisations(user.nickname)
-          .then(async (success) => {
-            const jwt = await fetchIdToken();
-            success.forEach(async (org) => {
-              const data = {
-                OrganisationId: org.id.toString(),
-                UserId: user.sub,
-              };
-              await setUserOrganisation(data, jwt).catch((err) => {
-                console.error('error: ' + err);
-              });
-            });
-          })
-          .catch((err) => {
-            trackException(err, 3);
-          })
-      : null;
-  };
   const currentPage =
     typeof window !== 'undefined'
       ? window.location.pathname.split('/').pop()

387ab2c734167a1f353137ae3cbc0aae177813b7
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-11-21 11:01:25 +0800

    passed location param to widget component, reduced number of rules to 5

--- src/components/side-bar/side-bar.js
+++ src/components/side-bar/side-bar.js
@@ -32,8 +32,7 @@ const SideBar = ({ ruleTotalNumber, hideCount }) => {
       </section>
 
       <section id="widget">
-        {/*TODO: Fix this - Currently removing to stop this from working until this is fixed  */}
-        <RulesWidget />
+        <RulesWidget location={process.env.SITE_URL} numberOfRules={5} />
       </section>
       <section>
         <h4>Why all these rules?</h4>

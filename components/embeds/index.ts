4e9e4cee5ca6cb7d40fca1c67baaea4b8ca9533b
Chloe Lin <chloelin@ssw.com.au>
2025-05-20 15:40:46 +0200

    add aside component

--- components/embeds/index.ts
+++ components/embeds/index.ts
@@ -2,16 +2,19 @@ import { Template } from "tinacms";
 import { emailEmbedComponent,  emailEmbedTemplate } from "./emailEmbed";
 import { figureEmbedComponent,  figureEmbedTemplate } from "./figureEmbed";
 import { imageEmbedComponent,  imageEmbedTemplate } from "./imageEmbed";
+import { asideEmbedComponent, asideEmbedTemplate } from "./asideEmbed";
 
 
 export const embedComponents={
     ...emailEmbedComponent,
     ...imageEmbedComponent,
-    ...figureEmbedComponent
+    ...figureEmbedComponent,
+    ...asideEmbedComponent
 }
 
 export const embedTemplates:Template[]=[
     emailEmbedTemplate,
     imageEmbedTemplate,
-    figureEmbedTemplate
+    figureEmbedTemplate,
+    asideEmbedTemplate
 ]

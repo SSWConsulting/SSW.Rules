dcba8a3be6a708e7e4c84a3b6d3fb53c85b831ed
Chloe Lin <chloelin@ssw.com.au>
2025-05-21 09:26:18 +0200

    added youtubeEmbed component

--- components/embeds/index.ts
+++ components/embeds/index.ts
@@ -3,18 +3,21 @@ import { emailEmbedComponent,  emailEmbedTemplate } from "./emailEmbed";
 import { figureEmbedComponent,  figureEmbedTemplate } from "./figureEmbed";
 import { imageEmbedComponent,  imageEmbedTemplate } from "./imageEmbed";
 import { asideEmbedComponent, asideEmbedTemplate } from "./asideEmbed";
+import { youtubeEmbedComponents, youtubeEmbedTemplate } from "./youtubeEmbed";
 
 
 export const embedComponents={
     ...emailEmbedComponent,
     ...imageEmbedComponent,
     ...figureEmbedComponent,
-    ...asideEmbedComponent
+    ...asideEmbedComponent,
+    ...youtubeEmbedComponents
 }
 
 export const embedTemplates:Template[]=[
     emailEmbedTemplate,
     imageEmbedTemplate,
     figureEmbedTemplate,
-    asideEmbedTemplate
+    asideEmbedTemplate,
+    youtubeEmbedTemplate
 ]

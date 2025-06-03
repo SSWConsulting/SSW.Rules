470211eb6e826be956244f0b9c9bdee9a1e67031
Gilles Pothieu <gillespothieu@ssw.fr>
2025-06-03 16:59:45 +0200

    Cleanup Algolia searchbar

--- app/rules/search/page.tsx
+++ app/rules/search/page.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import Layout from '@/components/layout/layout';
 import { Section } from '@/components/layout/section';
-import RulesSearchClient from '@/components/RulesSearchClient';
+import SearchBar from '@/components/SearchBar';
 
 export const revalidate = 300;
 
-export default async function RulesSearchPage({
-  searchParams,
-}: {
-  searchParams: { keyword?: string };
-}) {
+export default async function RulesSearchPage({ searchParams }: { searchParams: { keyword?: string } }) {
+    const { keyword = '' } = await searchParams;
+    const layout = await Layout({
+        children: (
+            <Section>
+                <SearchBar keyword={keyword} />
+            </Section>
+        ),
+    });
 
-      const { keyword = '' } = await searchParams;
-  const layout = await Layout({
-    children: (
-      <Section>
-        <RulesSearchClient keyword={keyword} />
-      </Section>
-    )
-  });
-
-  return layout;
+    return layout;
 }

43a37a01d2fedb43864ed4be27878baf2438d6c7
Hugo Pernet <hugopernet@ssw.fr>
2025-06-12 11:51:44 +0200

    using Suspense because of the useSearchParams in the client page

--- app/rules/search/page.tsx
+++ app/rules/search/page.tsx
@@ -1,15 +1,18 @@
-import Layout from '@/components/layout/layout';
-import { Section } from '@/components/layout/section';
-import RulesSearchClientPage from './client-page';
+import Layout from "@/components/layout/layout";
+import { Section } from "@/components/layout/section";
+import RulesSearchClientPage from "./client-page";
+import { Suspense } from "react";
 
 export const revalidate = 300;
 
 export default function RulesSearchPage() {
-    return (
-        <Layout>
-            <Section>
-                <RulesSearchClientPage />
-            </Section>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Section>
+        <Suspense fallback={null}>
+          <RulesSearchClientPage />
+        </Suspense>
+      </Section>
+    </Layout>
+  );
 }

50c77c40d5745058b9b2b665666b9922395ef1dc
Gilles Pothieu <gillespothieu@ssw.fr>
2025-06-20 10:25:53 +0200

    Adapting and factorization of the prepare-content.js script to fetch 2 news categories json file (category-uri-title-map.json and rule-to-categories.json)

--- scripts/prepare-content.js
+++ scripts/prepare-content.js
@@ -4,6 +4,19 @@ import { dirname, resolve, join } from 'path';
 import 'dotenv/config';
 import { execSync } from 'child_process';
 
+const copyAndMoveJsonFile = (fileName, scriptsPath) => {
+  const output = join(scriptsPath, fileName);
+  const dest = resolve(__dirname, `../${fileName}`);
+
+  execSync(`python "${input}"`, { stdio: 'inherit', cwd: scriptsPath });
+
+  if (!fs.existsSync(output)) {
+    console.error(`Expected file not found: ${output}`);
+    process.exit(1);
+  }
+  fs.copyFileSync(output, dest);
+}
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -16,15 +29,6 @@ if (!relPath) {
 const contentAbsPath = resolve(__dirname, relPath);
 const scriptsPath = join(contentAbsPath, 'scripts/tina-migration');
 const input = join(scriptsPath, 'build-rule-category-map.py');
-const output = join(scriptsPath, 'rule-category-mapping.json');
 
-const dest = resolve(__dirname, '../rule-category-mapping.json');
-
-execSync(`python "${input}"`, { stdio: 'inherit', cwd: scriptsPath });
-
-if (!fs.existsSync(output)) {
-  console.error(`Expected file not found: ${output}`);
-  process.exit(1);
-}
-
-fs.copyFileSync(output, dest);
+copyAndMoveJsonFile("category-uri-title-map.json", scriptsPath)
+copyAndMoveJsonFile("rule-to-categories.json", scriptsPath)

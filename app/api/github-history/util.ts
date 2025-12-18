719fe5fda1deac155e636a74baaba2e55b0304fc
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-12-18 16:29:19 +1100

    Ignoring sha (#336)
    ## Description
    ✏️ 
    - XXX
    - YYY
    
    ## Screenshot (optional)
    ✏️ 
    
    <!-- 
    Check out the relevant rules
    -
    https://www.ssw.com.au/rules/use-pull-request-templates-to-communicate-expectations/
    - https://www.ssw.com.au/rules/rules-to-better-pull-requests
    - https://www.ssw.com.au/rules/write-a-good-pull-request
    - https://www.ssw.com.au/rules/over-the-shoulder-prs 
    - https://www.ssw.com.au/rules/do-you-use-co-creation-patterns
    -->

--- app/api/github-history/util.ts
+++ app/api/github-history/util.ts
@@ -21,6 +21,7 @@ const CACHE_TTL = 3600; // 1 hour in seconds
  * These commits will be skipped, and the next non-excluded commit will be shown instead
  */
 export const EXCLUDED_COMMIT_SHAS: string[] = [
+  "6d7c8acbf37377fbc647e431f98cf58c3447b031",
   "e6a4f720c383f7013fd009a0aa6b52e8fe779ee2",
   "0a57480c7730685582dd67f12d3e49a12ce0af32",
 ];

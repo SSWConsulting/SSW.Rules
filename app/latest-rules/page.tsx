27ffeda8c1ed2bf68f3ab1b38217d26ebecaa52a
Gilles Pothieu [SSW] <gillespothieu@ssw.fr>
2026-01-05 05:05:15 +0100

    Added the default size of the page at 50 (#2262)
    Co-authored-by: Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>

--- app/latest-rules/page.tsx
+++ app/latest-rules/page.tsx
@@ -5,7 +5,7 @@ import LatestRuleClientPage from "./client-page";
 
 export const revalidate = 300;
 
-const DEFAULT_SIZE = 5;
+const DEFAULT_SIZE = 50;
 const MAX_SIZE = 50;
 
 type LatestRulePageProps = {

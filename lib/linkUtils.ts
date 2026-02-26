aa5ab247570ef1f7c85f37af69d9335f211e25ef
Calinator444 <calebwilliams@ssw.com.au>
2026-02-26 12:50:07 +1100

    added jsdoc for linkutils

--- lib/linkUtils.ts
+++ lib/linkUtils.ts
@@ -8,8 +8,17 @@ export const isExternalSSWSite = (href: string): boolean => {
   return externalSSWSitePatterns.test(href);
 };
 
+/**
+ * Determines whether a given href should be treated as an external link.
+ *
+ * @param href - The URL or path to evaluate.
+ * @returns `true` if the link points outside the current site (including SSW external sites), `false` if it's on ssw.com.au/rules
+ * @example
+ * isExternalLink("https://www.ssw.com.au/rules/*")   // false
+ * isExternalLink("https://anydomain.com.au")         // true
+ * isExternalLink("/company")                         // true
+ */
 export const isExternalLink = (href: string): boolean => {
-  // i.e. href = https://anydomain.com.au => true | href = https://www.ssw.com.au/rule/* => true for SSW External Site | href = /company => false for relative path
   if(href.includes("ssw.com.au/rules"))
   {
     return false;

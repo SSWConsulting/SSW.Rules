b508a9be6038265c2f2a4afc75050e1ddd552171
Chloe Lin <chloelin@ssw.com.au>
2026-03-24 13:39:06 +0100

    Removes unused activity-related code

--- app/activity-actions.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { getCachedDiscussionData, DiscussionData } from "@/lib/services/github/discussions.service";
-
-export async function getActivityData(): Promise<DiscussionData> {
-  return getCachedDiscussionData();
-}
--- app/activity-rules/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-// The activity-rules experience has moved to the homepage as an alternative view.
-export default function ActivityRulesPage() {
-  redirect("/");
-}
